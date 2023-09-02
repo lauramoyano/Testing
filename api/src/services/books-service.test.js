98af09ede2ce584786f98517442909be079d253a
lauramoyano <laura.moyano.go@gmail.com>
2023-09-01 23:42:45 -0500

    books service

---
+++ api/src/services/books-service.test.js
@@ -0,0 +1,48 @@
+const BooksService = require('./books.service');
+
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Harry Potter',
+  },
+];
+
+const mockGetAll = jest.fn();
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockGetAll,
+  create: () => {},
+})));
+
+describe('Test for BooksService', () => {
+  let service;
+  beforeEach(() => {
+    service = new BooksService();
+    jest.clearAllMocks();
+  });
+
+  describe('test for getBooks', () => {
+    test('should return a list book', async () => {
+      // Arrange
+      mockGetAll.mockResolvedValue(fakeBooks);
+      // Act
+      const books = await service.getBooks({});
+      console.log(books);
+      // Assert
+      expect(books.length).toEqual(1);
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
+    });
+
+    test('should return a list book', async () => {
+      mockGetAll.mockResolvedValue([{
+        _id: 1,
+        name: 'Harry putter 2',
+      }]);
+      const books = await service.getBooks({});
+      console.log(books);
+      expect(books[0].name).toEqual('Harry putter 2');
+    });
+  });
+});
